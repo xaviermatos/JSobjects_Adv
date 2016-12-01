3c7cbb239ea6f51934d1858cd2403e4ab8e00df0
xaviermatos <xmatos@aoiths.org>
2016-12-01 23:06:03 +0000

    1,2,3 done

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,13 +1,13 @@
 //Task 1: Console the year in which the song "Beat it" was relased 
-
+console.log(library.music.eighties.Michael_Jackson[0].year)
 
 //Task 2: Console the name of the song Lady Gaga released in 2006 
-
+console.log(library.music.two_thousands.Lady_Gaga[0].song)
 
 
 $( document ).ready(function() {
 //Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
-    
+    $('#roll').append(library.music.twenty_tens.Adele[0].song);
 //Task 4: (project) Create a site that can display songs from an artist specified by a user search.
 
 });
